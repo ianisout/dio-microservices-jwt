46ed07ab91128f209d7fb8e9e13256f255f024a6
Ian LS <iansouza.sjc@gmail.com>
2022-08-17 15:04:49 -0300

    set up express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('asd')
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'bar ' });
+});
+
+app.listen(3000, () => console.log('Up and running on port 3000'));
