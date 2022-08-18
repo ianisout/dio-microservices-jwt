74fb60939e2bbabb217eb51a07563b81e1668126
Ian LS <iansouza.sjc@gmail.com>
2022-08-18 08:19:32 -0300

    add jwt

--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import JWT from 'jsonwebtoken';
 import ForbiddenError from '../models/errors/forbidden.error.model';
 import userRepository from '../repositories/user.repository';
 
@@ -14,24 +16,31 @@ authorizationRoute.post(
     try {
       const authorizationHeader: any = req.headers['authorization'];
 
-      
       if (!authorizationHeader) throwError('Unauthorized');
-      
+
       const [authType, token] = authorizationHeader.split(' ');
-      
+
       if (authType !== 'Basic' || !token) throwError('Invalid authentication');
-      
+
       const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
       const [username, password] = tokenContent.split(':');
-      
+
       if (!username || !password) throwError('Missing credentials');
-      
+
       const user = await userRepository.findByUsernameAndPassword(
         username,
         password
-        );
+      );
 
-      console.log(user);
+      if (!user) throwError('Bad credentials');
+
+      const jwtPayload = { username: user?.username };
+      const jwtOptions = { subject: user?.uuid };
+      const secretKey = 'my_secret_key';
+
+      const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
+
+      res.status(StatusCodes.OK).json({ token: jwt });
     } catch (error) {
       next(error);
     }
